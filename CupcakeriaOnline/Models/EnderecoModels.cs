1d1caa9829235e603e9b22e85f826ad17388c593
fabiocsantana <fabio.fcsantana@gmail.com>
2015-09-29 20:17:16 -0300

    Testando commit

---
+++ CupcakeriaOnline/Models/EnderecoModels.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+using System.Web.Mvc;
+using System.Linq;
+using System.ComponentModel;
+
+namespace CupcakeriaOnline.Models
+{
+    public class EnderecoModels
+    {
+        [Table("Endereco")]
+        public class EnderecoCliente
+        {
+            [Key]
+            [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+            public int EnderecoId { get; set; }
+            
+            public int ClienteId { get; set; }
+
+            [Required(ErrorMessage = "CEP obrigatório")]
+            [StringLength(8)]
+            [DisplayFormat(DataFormatString = "{0:#####-###}")]
+            [DisplayName("CEP")]
+            public string EnderecoCep { get; set; }
+
+            [Required(ErrorMessage = "Logradouro obrigatório")]
+            [DisplayName("Logradouro")]
+            public string EnderecoLogr { get; set; }
+
+            [DisplayName("Nº")]
+            public double EnderecoNum { get; set; }
+
+            [DisplayName("Complemento")]
+            public string EnderecoCompl { get; set; }
+
+            [Required, DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+            public DateTime EnderecoDtCad { get; set; }
+        }
+    }
+}
