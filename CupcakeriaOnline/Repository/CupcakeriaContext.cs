c8a9bdfab74c0ac7ad057a2a1513264c3f143698
Andre <andmiranda15@gmail.com>
2015-10-06 21:32:48 -0300

    Inclusão da classe context

---
+++ CupcakeriaOnline/Repository/CupcakeriaContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CupcakeriaOnline.Repository
+{
+    public class CupcakeriaContext
+    {
+
+    }
+}
--- CupcakeriaOnline/Repository/PedidoRepository.cs
+++ CupcakeriaOnline/Repository/PedidoRepository.cs
@@ -1,11 +1,35 @@
-﻿using System;
+﻿using CupcakeriaOnline.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 
 namespace CupcakeriaOnline.Repository
 {
     public class PedidoRepository
     {
+        private PedidoRepository. context = new PedidoRepository.();
+
+        public void Salva()
+        {
+            context.SaveChanges();
+        }
+
+        public void Adiciona(PedidoModel.PedidoCupcake pedido)
+        {
+            context.Pedidos.Add(pedido);
+        }
+
+        public void Remove(int id)
+        {
+            PedidoModel.PedidoCupcake pedido = context.Pedidos.Find(id);
+            context.Pedidos.Remove(pedido);
+        }
+
+        public PedidoModel.PedidoCupcake Busca(int id)
+        {
+            return context.Pedidos.Find(id);
+        }
     }
 }
