87205568605ef298652cb011090e4a478a86957a
Andre <andmiranda15@gmail.com>
2015-10-06 21:20:47 -0300

    Alteração das models

---
+++ CupcakeriaOnline/Models/Item_Pedido.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+using System.Web.Mvc;
+using System.Linq;
+using System.ComponentModel;
+
+namespace CupcakeriaOnline.Models
+{
+        [Table("Item Pedido")]
+        public class ItemPedidoCupcake
+        {
+            [Key]
+            [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+            public int pk_idItemPedido { get; set; }
+
+            [Key]
+            [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+            public int fk_idCupcake { get; set; }
+
+            [Required, DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+            [DisplayName("Quantidade de Cupcake")]
+            public int qtdeItem { get; set; }
+
+            [Required, DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+            [DisplayName("Valor do Cupcake")]
+            public double valorItem { get; set; }
+        }
+}
---
+++ CupcakeriaOnline/Models/Pedido.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+using System.Web.Mvc;
+using System.Linq;
+using System.ComponentModel;
+
+namespace CupcakeriaOnline.Models
+{
+        [Table("Pedido")]
+        public class PedidoCupcake
+        {
+            [Key]
+            [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+            public int pk_idPedido { get; set; }
+
+            [Key]
+            [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+            public int fk_idCliente { get; set; }
+
+            [Required, DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+            [DisplayName("Status")]
+            public bool statusPedido { get; set; }
+
+            [Required, DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+            [DisplayName("Valor Total")]
+            public double vlrTotalPedido { get; set; }
+
+            [Required, DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+            public DateTime dtRealizadoPedido { get; set; }
+
+            [Required, DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+            public DateTime dtEntregaPedido { get; set; }
+    }
+}
