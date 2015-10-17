2e0356cf7d84261a65956d3da01d83bab938981c
andresanm <andmiranda15@gmail.com>
2015-10-17 02:20:35 -0300

    Repository Item_Pedido, Pedido Atualizado

--- CupcakeriaOnline/Repository/Item_PedidoRepository.cs
+++ CupcakeriaOnline/Repository/Item_PedidoRepository.cs
@@ -2,10 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using CupcakeriaOnline.Models;
+
 
 namespace CupcakeriaOnline.Repository
 {
     public class Item_PedidoRepository
     {
+        private CupcakeriaContext context = new CupcakeriaContext();
+
+        public void Salva()
+        {
+            context.SaveChanges();
+        }
+
+        public void Adiciona(Item_PedidoModel itempedido)
+        {
+            context.Item_Pedidos.Add(itempedido);
+        }
+
+        public void Remove(int id)
+        {
+            Item_PedidoModel itempedido = context.Item_Pedidos.Find(id);
+            context.Item_Pedidos.Remove(itempedido);
+        }
+
+        public Item_PedidoModel Busca(int id)
+        {
+            return context.Item_Pedidos.Find(id);
+        }
     }
 }
