27631150b86ccaa3f77178f7cc72691ebfa4bd84
Antonio Artur Meissner Neto <arturmnt@gmail.com>
2015-10-20 20:18:22 -0200

    Adicionado classe Model Cupcake_Pedido
    Adicionado classe Model Cupcake_Pedido

---
+++ CupcakeriaOnline/Models/Cupcake_Pedido.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+using System.Web.Mvc;
+using System.Linq;
+using System.ComponentModel;
+
+namespace CupcakeriaOnline.Models
+{
+    [Table("Cupcake Pedido")]
+    public class Cupcake_Pedido
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int pk_idCupcake { get; set; }
+
+        [ForeignKey("fk_idMassa")]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int fk_idMassa { get; set; }
+
+        [ForeignKey("fk_idRecheio")]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int fk_idCobertura {get; set;}
+
+        [Required, DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        [DisplayName("Valor do Cupcake")]
+        public double valorCupcake {get; set;}
+
+    }
+}
