cea98a2579b5d29d93524dd37b9ca0412bc57e9f
fabiocsantana <fabio.fcsantana@gmail.com>
2015-11-08 17:40:12 -0200

    Incluindo DbSet da Model Administracao

--- CupcakeriaOnline/Repository/CupcakeriaContext.cs
+++ CupcakeriaOnline/Repository/CupcakeriaContext.cs
@@ -9,20 +9,28 @@ namespace CupcakeriaOnline.Repository
 {
     public class CupcakeriaContext : DbContext
     {
+        public DbSet<AdministracaoModel> AdministracaoModels { get; set; }
+
+        public DbSet<ClienteModel> Cliente { get; set; }
+
+        public DbSet<EnderecoModel> Endereco { get; set; }
+
+        public DbSet<PedidoModel> Pedidos { get; set; }
+
+        public DbSet<Item_PedidoModel> Item_Pedidos { get; set; }
+        
+        public DbSet<MassaModel> Massa { get; set; }
+
+        public DbSet<CoberturaModel> Coberturas { get; set; }
+
+        public DbSet<RecheioModel> Recheios { get; set; }
+
         public CupcakeriaContext()
             : base("name=CadastroContext")
         {
 
-            Database.Connection.ConnectionString = @"Data Source=LAB03-12;Initial Catalog=DB_CUPCAKERIA;Integrated Security=True";
+            Database.Connection.ConnectionString = @"<!-- INSERIR CONNECTION STRING >";
 
-        } 
-
-        public DbSet<CoberturaModel> Coberturas { get; set; }
-        public DbSet<RecheioModel> Recheios { get; set; }
-        public DbSet<Item_PedidoModel> Item_Pedidos { get; set; }
-        public DbSet<PedidoModel> Pedidos { get; set; }
-        public DbSet<ClienteModel> Cliente { get; set; }
-        public DbSet<EnderecoModel> Endereco { get; set; }
-        public DbSet<MassaModel> Massa { get; set; }
+        }     
     }
 }
