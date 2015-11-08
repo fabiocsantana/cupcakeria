644e682c731a6e01be34ce721dd696691d72f4ed
fabiocsantana <fabio.fcsantana@gmail.com>
2015-11-08 17:33:15 -0200

    Model Administrador

---
+++ CupcakeriaOnline/Models/AdministradorModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+using System.Web.Mvc;
+
+namespace CupcakeriaOnline.Models
+{
+    [Table("Administrador")]
+    public class AdministradorModel
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int pk_idAdministrador { get; set; }
+
+        [Required(ErrorMessage = "Nome obrigatório")]
+        [DisplayName("Nome")]
+        public string nomeAdministrador { get; set; }
+    }
+}
